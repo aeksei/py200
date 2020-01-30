2eccc68aa55e7e8420a54283ab0672617d036f6f
alekseyp <aeksei94@gmail.com>
2020-01-30 21:21:53 +0300

    final

--- lesson_30_01_2020/py200_1_1.py
+++ lesson_30_01_2020/py200_1_1.py
@@ -62,7 +62,7 @@ class GlassDefaultArg:
 
         if isinstance(occupied_volume, (int, float)):
             if occupied_volume >= 0:
-                self.occupied_volume = 0  # заполнненость водой
+                self.occupied_volume = occupied_volume  # заполнненость водой
             else:
                 raise ValueError
         else:
@@ -97,15 +97,61 @@ glass_4_2 = GlassDefaultListArg(200)
 #    Вызовите методы add_water и remove.
 #    Убедитесь, что методы правильно изменяют атрибут occupied_volume.
 
+class GlassAddRemove:
+    def __init__(self, capacity_volume, occupied_volume=0):
+        if isinstance(capacity_volume, (int, float)):
+            if capacity_volume > 0:
+                self.capacity_volume = capacity_volume  # объем стакана
+            else:
+                raise ValueError
+        else:
+            raise TypeError
+
+        if isinstance(occupied_volume, (int, float)):
+            if occupied_volume >= 0:
+                self.occupied_volume = occupied_volume  # заполнненость водой
+            else:
+                raise ValueError
+        else:
+            raise TypeError
+
+    def add_water(self, adding_water):
+        space = self.capacity_volume - self.occupied_volume  # объем пустого места в стакане
+        if adding_water <= space:
+            self.occupied_volume += adding_water
+            return self.occupied_volume
+        else:
+            self.occupied_volume = self.capacity_volume
+            return adding_water - space
+
+    def remove_water(self, removing_water):
+        pass
 
 
-# 6. Создайте три объекта типа GlassAddRemove, 
+glass_5_1 = GlassAddRemove(200, 100)
+print(glass_5_1.add_water(50) == 150)
+
+# 6. Создайте три объекта типа GlassAddRemove,
 #    вызовите функцию dir для трёх объектов и для класса GlassAddRemove.
 #    а. Получите типы объектов и класса
 #    б. Проверьте тип созданного объекта.
 
+print("======")
+
+glass_6_1 = GlassAddRemove(200)
 
 
+if isinstance(glass_6_1, GlassAddRemove):
+    print(type(int))
+
+GlassAddRemove(200)
+# GlassAddRemove.__init__(self, capacity_volume=200, occupied_volume=0)
+
+print(dir(glass_6_1))
+print(glass_6_1.__dict__)
+
+print(dir(GlassAddRemove))
+print(GlassAddRemove.__dict__)
 
 # ---------------------------------------------------------------------------------------------
 # Внутренние объекты класса (стр. 25-33)
@@ -115,11 +161,28 @@ glass_4_2 = GlassDefaultListArg(200)
 #    а также после создания объекта.
 #    Опишите результат.
 
+class GlassAddRemove:
+    def __init__(self, capacity_volume, occupied_volume=0):
+        # print()
+        self.capacity_volume = capacity_volume  # объем стакана
+        # print()
+        self.occupied_volume = occupied_volume  # заполнненость водой
+        # print()
 
 # 8. Создайте три объекта Glass. (стр. 27)
 #    Получите id для каждого объекта с соответсвующим id переменной self.
 
+class GlassId:
+    def __init__(self, capacity_volume, occupied_volume=0):
+        self.capacity_volume = capacity_volume  # объем стакана
+        self.occupied_volume = occupied_volume  # заполнненость водой
+        print(hex(id(self)))  # адрес  в памяти объекта self
 
+glass_8_1 = GlassId(200)
+print(hex(id(glass_8_1)))
+
+glass_8_2 = GlassId(200)
+print(hex(id(glass_8_2)))
 
 # 9. Корректно ли следующее объявление класса с точки зрения:
 #     - интерпретатора Python;
@@ -137,10 +200,16 @@ d.print_me(d())
 
 # 10. Исправьте
 class A:
-	def __init__(self, a):
-		if 10 < a < 50:
-			return
-		self.a = a;	
+    def __init__(self, a):
+        if 10 < a < 50:
+            return
+        self.a = a;
+
+obj_1 = A(25)
+print(dir(obj_1))
+
+obj_2 = A(100)
+print(dir(obj_2))
 
 # Объясните так реализовывать __init__ нельзя?
 		
@@ -152,13 +221,24 @@ class A:
 
 class Node:
     def __init__(self, prev=None, next_=None):
+        """
+        :param prev: Class Node
+        :param next_: Class Node
+        """
         self.__prev = prev
         self.__next = next_
+
     def set_next(self, next_):
         self.__next = next_
 
     def set_prev(self, prev):
         self.__prev = prev
+
+    def get_next(self):
+        return self.__next
+
+    def get_prev(self):
+        return self.__prev
         
     def __str__(self):
         ...
@@ -167,8 +247,20 @@ class Node:
         ...
 
 class LinkedList:
+    def __init_(self, nodes=None):
+        if nodes is None:
+            self.head = None
+            self.tail = None
+            self.__len = 0
 
-
+        elif isinstance(nodes, Node):
+            self.head = nodes
+            self.tail = nodes
+            self.__len = 1
+        else:
+            self.head = nodes[0]
+            self.tail = nodes[-1]
+            self.__len = len(nodes)
 
     def insert(self, node, index=0):
         '''
@@ -176,8 +268,42 @@ class LinkedList:
         node - Node
         index - position of node
         '''
-        pass
-        
+        # TODO index out of range
+
+        if index == 0:
+            node.set_prev(None)
+            node.set_next(self.head)
+            self.head.set_prev(node)
+            self.head = node
+        elif index == (self.__len - 1):
+            node.set_next(None)
+            node.set_prev(self.tail)
+            self.tail.set_next(node)
+            self.tail = node
+        else:
+            current_node = self.head
+            for i in range(index):
+                current_node = current_node.get_next()
+            current_node_next = current_node.get_next()
+
+            current_node.set_next(node)
+            node.set_prev(current_node)
+
+            node.set_next(current_node_next)
+            current_node_next.set_prev(node)
+
+
+
+
+        # current_node = self.head
+        # for i in range(index):
+        #     current_node = current_node.get_next()
+
+
+        current_node.set_prev(node)
+        node.set_next(self.head)
+
+
        
     def append(self, node):
         '''
